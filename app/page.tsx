05f2060bd92b8c08cdec72bdde1ebe2ea0644766
degendevelopmentstudios <degendevelopmentstudios@gmail.com>
2025-04-16 18:42:32 -0700

    Update page.tsx
    Fix & update with NFT Rarity

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Header from "../components/header"
 import HeroSection from "../components/hero-section"
 import AboutSection from "../components/about-section"
 import TokenomicsSection from "../components/tokenomics-section"
-import NFTSections from "../components/nft-sections"
 import RoadmapSection from "../components/roadmap-section"
 import MemeSection from "../components/meme-section"
 import Footer from "../components/footer"
@@ -14,7 +13,6 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <TokenomicsSection />
-      <NFTSections />
       <RoadmapSection />
       <MemeSection />
       <Footer />
